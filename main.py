17abd8e94828bb42da84a83ee3a2f0532d9d4e20
Nurulloh <nurilloxdigital@example.com>
2025-06-30 15:22:31 +0300

    ✅ main.py faylida ParseMode import yo‘li to‘g‘rilandi

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import ParseMode
+from aiogram.enums import ParseMode
 from aiogram.client.bot import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import TOKEN
 from database.db import create_users_table
 from utils.scheduler import setup_scheduler
-
 from handlers import start, add_task, view_tasks, delete_task, settings, admin
 
 async def main():
@@ -15,7 +14,7 @@ async def main():
     storage = MemoryStorage()
 
     # 🤖 Bot va Dispatcher
-    bot = Bot(token=TOKEN)
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher(storage=storage)
 
     # 🗃 Jadvalni yaratamiz
@@ -36,5 +35,5 @@ async def main():
     print("✅ Bot ishga tushdi...")
     await dp.start_polling(bot)
 
-if name == "__main_":
+if __name__ == "__main__":
     asyncio.run(main())
